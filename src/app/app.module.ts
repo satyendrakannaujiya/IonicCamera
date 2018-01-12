eb5a3f1915dd1dcf41b0a5c19f634f0c82c783a5
satyendrakannaujiya <satyendra250896@gmail.com>
2018-01-12 22:05:22 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
+import { Camera } from "@ionic-native/camera";
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Camera,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,67 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Camera, CameraOptions } from "@ionic-native/camera";
+import { AlertController } from "ionic-angular";
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  //NAME = "trash";
+  public photos: any;
+  public base64Image: string;
+  constructor(
+    public navCtrl: NavController,
+    private camera: Camera,
+    private alertCtrl: AlertController
+  ) {}
 
-  constructor(public navCtrl: NavController) {
-
+  ngOnInit() {
+    this.photos = [];
   }
 
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+        this.photos.push(this.base64Image);
+        this.photos.reverse();
+      },
+      err => {
+        // Handle error
+      }
+    );
+  }
+  deletePhoto(index) {  
+    let alert = this.alertCtrl.create({
+      title: "warning",
+      message: "are you sure",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler() {
+            console.log("cancel clicked");
+          }
+        },
+        {
+          text: "Yes",
+          role: "sure",
+          handler() {
+            this.photos.splice(index,1);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
